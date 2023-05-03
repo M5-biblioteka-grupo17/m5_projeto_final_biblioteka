f3131d5cf7a87ed6fefe2d455fd0a935daa5fbd3
Joseph <josephvriesman16@gmail.com>
2023-05-03 19:32:33 -0300

    feat: criação do serializer de loan

--- copies/serializers.py
+++ copies/serializers.py
@@ -3,6 +3,8 @@ from rest_framework.validators import UniqueValidator
 from .models import Copy
 from books.models import Book
 from django.forms.models import model_to_dict
+from users.serializers import UserSerializer
+from .models import Loan
 
 class CopySerializer(serializers.ModelSerializer):
     book_id = serializers.CharField(
@@ -23,3 +25,10 @@ class CopySerializer(serializers.ModelSerializer):
     class Meta:
         model = Copy
         fields = ["id", "book_id", "amount", "available", "reserved_copy"]
+
+class loanSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = Loan
+        fields = ["id", "user", "copy", "start_date", "return_date", "is_returned"]
+        read_only_fields = ["id", "user", "copy", "start_date", "return_date", "is_returned"]
