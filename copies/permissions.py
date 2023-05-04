bcd9dcbaef72f19e6e13899c3968d9cb665e3079
Joseph <josephvriesman16@gmail.com>
2023-05-04 09:42:16 -0300

    fix: arrumando view e serializer de copies

--- copies/permissions.py
+++ copies/permissions.py
@@ -1,7 +1,11 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class isCollaboratorOrGet(BasePermission):
     def has_permission(self, request, view):
-        if request.method == "GET":
+        if request.method in SAFE_METHODS:
             return True
+        return request.user.is_authenticated and request.user.library_collaborator
+    
+class isCollaborator(BasePermission):
+    def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.library_collaborator
